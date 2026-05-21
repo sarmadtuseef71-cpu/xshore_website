42e5abd37ed11629a93d760e4ef6bbba4f4a3660
Sarmad Tuseef <sarmadtuseef58@gmail.com>
2026-05-21 11:00:21 +0500

    fix: make rental redirect permanent

--- src/routes/rental.tsx
+++ src/routes/rental.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/rental")({
   beforeLoad: () => {
-    throw redirect({ to: "/equipment-rental", replace: true });
+    throw redirect({ to: "/equipment-rental", replace: true, statusCode: 301 });
   },
 });
